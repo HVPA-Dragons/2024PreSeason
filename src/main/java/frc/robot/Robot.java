de6ac097491487dd7f82c786b7a63ada9762b572
Jacob M. Frost <jacob.frost@uboces.edu>
2024-03-15 10:12:03 -0400

    Getting ready for testing
    Fixed issues with imports into Robot.java
    
    Will be testing at CTE today

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 //import edu.wpi.first.cameraserver.CameraServer;
 
@@ -18,6 +19,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
         new RobotContainer();
+        m_robotContainer = new RobotContainer();
 
     }
 
@@ -48,6 +50,8 @@ public class Robot extends TimedRobot {
      * This autonomous runs the autonomous command selected by your
      * {@link RobotContainer} class.
      */
+    private RobotContainer m_robotContainer;
+
     @Override
     public void autonomousInit() {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
